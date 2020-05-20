a71891e78985e18ed902f770a2c80ade7f4a2e4f
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-20 20:07:35 -0300

    Programa finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,9 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		double largura, altura;
+		int opcao = 1;
 
+		// leitura da largura com verificação
 		System.out.print("Digite a largura do retangulo: ");
 		largura = sc.nextDouble();
 		while (largura < 1) {
@@ -19,12 +21,41 @@ public class Program {
 				largura = sc.nextDouble();
 			}
 		}
+		// leitura da altura com verificação
 		System.out.print("Digite a altura do retangulo: ");
 		altura = sc.nextDouble();
 		while (altura < 1) {
 			if (altura < 1) {
 				System.out.print("O valor deve ser positivo. Tente novamente: ");
 				altura = sc.nextDouble();
+			}
+		}
+
+		while (opcao != 3) {
+			// menu
+			System.out.println();
+			System.out.println("MENU:");
+			System.out.println("1 - Mostrar area");
+			System.out.println("2 - Mostrar perimetro");
+			System.out.println("3 - Sair");
+			System.out.print("Digite uma opcao: ");
+			opcao = sc.nextInt();
+
+			System.out.println();
+
+			switch (opcao) {
+			case 1:
+				System.out.printf("AREA = %.1f\n", largura * altura);
+				break;
+			case 2:
+				System.out.printf("PERIMETRO = %.1f\n", (largura * 2) + (altura * 2));
+				break;
+			case 3:
+				System.out.println("FIM DO PROGRAMA!");
+				break;
+			default:
+				System.out.println("OPCAO INVALIDA");
+				break;
 			}
 		}
 
